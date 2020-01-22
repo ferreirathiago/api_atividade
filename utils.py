6f329f0e7e9634eaddd3ba5e0ddb9ad14cf1b33a
unknown <ferreirathiago@me.com>
2020-01-21 21:35:29 -0300

    Adicionado models com tabelas de pessoas e atividades; Criado utils de teste para manipular os dados do SQLite

---
+++ utils.py
@@ -0,0 +1,29 @@
+from models import Pessoas, db_session
+
+def insere_pessoas():
+    pessoa = Pessoas(nome='Rafael', idade='30')
+    print (pessoa)
+    pessoa.save()
+
+def consulta_pessoa():
+    pessoa = Pessoas.query.all()
+    print(pessoa)
+    pessoa = Pessoas.query.filter_by(nome='Thiago')
+    #print(pessoa.idade)
+    for p in pessoa:
+        print(p.idade)
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Thiago').first()
+    pessoa.idade = 21
+    pessoa.save()
+
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Thiago').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoas()
+    #altera_pessoa()
+    exclui_pessoa()
+    consulta_pessoa()
